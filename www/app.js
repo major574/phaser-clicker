d0e2d6545e9b6269c6970f4481ca2d7aa660ca7c
major574 <mcjsmt@gmail.com>
2016-06-08 15:47:34 +0200

    added timers

--- www/app.js
+++ www/app.js
@@ -3,10 +3,13 @@ var game = new Phaser.Game(360, 500, Phaser.AUTO, 'clicker', { preload: preload,
 var grassSprite;
 var buildOne;
 var buildText;
+var mineText;
+var buildCount = [];
 var counter = 0;
 var button;
 var buildBool = false;
 var bmd;
+var mineCounter = 20;
 
 function preload() {
 
@@ -22,9 +25,10 @@ function create() {
     build1Button = game.add.sprite(40, 460, 'button1');
     build1Button.anchor.setTo(0.5, 0.5);
     build1Button.inputEnabled = true;
-
-    buildText = game.add.text(40, 16, "Buildings: " + 0, { fill: '#ffffff' });
     build1Button.events.onInputDown.add(listener, this);
+
+    buildText = game.add.text(20, 16, "Buildings: " + 0, {font: "16px Ariel", fill: '#ffffff' });
+    mineText = game.add.text(20, 40, "Energy: " + 20, {font: "16px Ariel", fill: "#ffffff" })
 
     buildOne = game.make.sprite(0,0, 'build1');
     buildOne.anchor.set(0.5)
@@ -32,6 +36,8 @@ function create() {
     bmd = game.add.bitmapData(game.width, game.height);
     bmd.addToWorld();
     bmd.smoothed = false;
+
+    game.time.events.loop(Phaser.Timer.SECOND, updateCounter, this);
 
 }
 
@@ -42,14 +48,22 @@ function listener () {
         console.log(buildBool);
     }
 }
+
 function clickGrass (pointer, x, y) {
-    if(buildBool === true && pointer.isDown) {
-        counter++;
+    if(buildBool === true && pointer.isDown && mineCounter > 10) {
+        buildCount.push(0);
+        counter = buildCount.length;
         buildText.text = "Buildings: " + counter;
         bmd.draw(buildOne, x, y);
+        mineCounter-= 10;
         buildBool = false;
         console.log(buildBool);
     }
 }
 
+function updateCounter () {
+    mineCounter+= buildCount.length;
+    mineText.setText('Energy: ' + mineCounter);
+
+}
 
